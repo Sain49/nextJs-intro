5dda5ef24f8bca738c9e5d1a5d47a4e4bb5a4546
Arif.A <arif.sain@gmail.com>
2025-08-14 17:00:08 +0200

    Rename 'strings' to 'tags' in Bagdes component for clarity

--- components/bagdes.tsx
+++ components/bagdes.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 
 export default function Bagdes({
-  strings,
+  tags,
   className,
 }: {
-  strings: string[];
+  tags: string[];
   className?: string;
 }) {
   return (
     <ul className="flex gap-1 flex-wrap">
-      {strings.map((tag, i) => (
+      {tags.map((tag, i) => (
         <li className={`text-sm px-2 py-0.5 ${className}`} key={i}>
           <Link href={`/recipes?tag=${tag}`}>{tag}</Link>
         </li>
