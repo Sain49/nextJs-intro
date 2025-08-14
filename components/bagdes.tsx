640888a7bd3b6b8996c8f64362a2931607f712b2
Arif.A <arif.sain@gmail.com>
2025-08-14 15:56:40 +0200

    Update Bagdes component to wrap tags in Link for navigation

--- components/bagdes.tsx
+++ components/bagdes.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Bagdes({
   strings,
   className,
@@ -9,7 +11,7 @@ export default function Bagdes({
     <ul className="flex gap-1 flex-wrap">
       {strings.map((tag, i) => (
         <li className={`text-sm px-2 py-0.5 ${className}`} key={i}>
-          {tag}
+          <Link href={`/recipes?tag=${tag}`}>{tag}</Link>
         </li>
       ))}
     </ul>
